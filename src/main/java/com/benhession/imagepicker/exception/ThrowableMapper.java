d89bf17a788975310652c3ccc5b08b7618143be5
Ben Hession <bshession@gmail.com>
2024-04-07 15:36:45 +0100

    Refactor validation messages.

--- src/main/java/com/benhession/imagepicker/exception/ThrowableMapper.java
+++ src/main/java/com/benhession/imagepicker/exception/ThrowableMapper.java
@@ -2,12 +2,11 @@ package com.benhession.imagepicker.exception;
 
 import jakarta.inject.Inject;
 import jakarta.ws.rs.core.Response;
-
 import jakarta.ws.rs.ext.ExceptionMapper;
 import jakarta.ws.rs.ext.Provider;
+import org.eclipse.microprofile.config.inject.ConfigProperty;
 import org.jboss.logging.Logger;
 
-import java.util.ResourceBundle;
 import java.util.UUID;
 
 @Provider
@@ -16,12 +15,14 @@ public class ThrowableMapper implements ExceptionMapper<Throwable> {
     @Inject
     Logger log;
 
+    @ConfigProperty(name = "validation-messages.system.error")
+    String systemErrorMessage;
+
     @Override
     public Response toResponse(Throwable e) {
         String errorId = UUID.randomUUID().toString();
         log.error("errorId[{}]", errorId, e);
-        String defaultErrorMessage = ResourceBundle.getBundle("ValidationMessages").getString("System.error");
-        ErrorResponse.ErrorMessage errorMessage = new ErrorResponse.ErrorMessage(defaultErrorMessage);
+        ErrorResponse.ErrorMessage errorMessage = new ErrorResponse.ErrorMessage(systemErrorMessage);
         ErrorResponse errorResponse = new ErrorResponse(errorId, errorMessage);
         return Response.status(Response.Status.INTERNAL_SERVER_ERROR).entity(errorResponse).build();
     }
