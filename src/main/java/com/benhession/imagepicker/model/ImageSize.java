97cea800f9251b918a762e22df35ca764789a5b3
Ben Hession <bshession@gmail.com>
2024-06-16 17:59:07 +0100

    revert enum changes

--- src/main/java/com/benhession/imagepicker/model/ImageSize.java
+++ src/main/java/com/benhession/imagepicker/model/ImageSize.java
@@ -1,19 +1,8 @@
 package com.benhession.imagepicker.model;
 
 public enum ImageSize {
-    THUMBNAIL("thumbnail"),
-    SMALL("small"),
-    MEDIUM("medium"),
-    LARGE("large");
-
-    private final String value;
-
-    ImageSize(String value) {
-        this.value = value;
-    }
-
-    @Override
-    public String toString() {
-        return value;
-    }
+    THUMBNAIL,
+    SMALL,
+    MEDIUM,
+    LARGE
 }
--- src/main/java/com/benhession/imagepicker/model/ImageType.java
+++ src/main/java/com/benhession/imagepicker/model/ImageType.java
@@ -1,19 +1,8 @@
 package com.benhession.imagepicker.model;
 
 public enum ImageType {
-    SQUARE("square"),
-    PANORAMIC("panoramic"),
-    RECTANGULAR("rectangular"),
-    LANDSCAPE("landscape");
-
-    private final String value;
-
-    ImageType(String value) {
-        this.value = value;
-    }
-
-    @Override
-    public String toString() {
-        return value;
-    }
+    SQUARE,
+    PANORAMIC,
+    RECTANGULAR,
+    LANDSCAPE
 }
