554a441114467ee7e4ce10dfe9d43a88551f6ecb
Ben Hession <bshession@gmail.com>
2024-06-23 17:59:20 +0100

    add list images endpoint

--- src/main/java/com/benhession/imagepicker/repository/ImageMetadataRepository.java
+++ src/main/java/com/benhession/imagepicker/repository/ImageMetadataRepository.java
@@ -2,8 +2,9 @@ package com.benhession.imagepicker.repository;
 
 import com.benhession.imagepicker.model.ImageMetadata;
 import io.quarkus.mongodb.panache.PanacheMongoRepository;
+import io.quarkus.panache.common.Page;
 import jakarta.enterprise.context.ApplicationScoped;
-
+import java.util.List;
 import java.util.Optional;
 
 @ApplicationScoped
@@ -11,5 +12,11 @@ public class ImageMetadataRepository implements PanacheMongoRepository<ImageMeta
     public Optional<ImageMetadata> findByParentKey(String key) {
         return find("parentKey", key)
                 .firstResultOptional();
+    }
+
+    public List<ImageMetadata> findPage(int page, int size) {
+        return findAll()
+          .page(Page.of(page, size))
+          .list();
     }
 }
