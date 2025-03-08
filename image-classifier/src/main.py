0491d7fcaab2e97dd69a387d0ad3abc740c4d765
Ben Hession <bshession@gmail.com>
2025-03-08 11:38:04 +0000

    remove test endpoints

--- image-classifier/src/main.py
+++ image-classifier/src/main.py
@@ -2,14 +2,12 @@ from awsgi import response
 from flask import Flask
 
 from ImageClassifierController import image_classifier_controller
-from webhooks import webhook
 
 
 def create_app():
   app = Flask(__name__)
 
-  # Import and register blueprints, if any
-  app.register_blueprint(webhook)
+  # Import and register blueprints
   app.register_blueprint(image_classifier_controller)
 
   return app
