0fa8f684c48a9fd832152fb31516308e215b5b84
Ben Hession <7346923+benhession@users.noreply.github.com>
2025-03-24 21:20:58 +0000

    Feature/add mongodb migrations (#27)
    * add mongo-migrate
    
    * test migrate in github workflow
    
    * use input instead of secret for database name
    
    * fix workflow
    
    * fix workflow
    
    * Move migration to merge following test
    
    * Update README
    
    * Add annotation for migrations during unit tests.
    
    * explicitly share mongo during tests
    
    * use available ports
    
    * use available ports
    
    * add shared properties
    
    * update quarkus
    
    * Use testcontainers directly instead of devservices
    
    * Add build reports for data
    
    * call npm ci in workflow
    
    * Use mongoDbAtlas in tests

---
+++ mongo-db-migrations/README.md
@@ -0,0 +1,25 @@
+# MongoDb Migrations
+
+Migrations are run on the pipline after a deployment. They can also be run during unit tests as
+described below
+
+## Adding a new migration
+
+To add a new migration, use the command `npx migrate-mongo create <migration name>`. This will
+create a migration template in the [migrations](./migrations) directory prepended with the current
+date + time.
+
+## Running the migration locally
+
+To test locally, the database connection can be setup using the environment variables `MONGO_URL`
+and `DATABASE_NAME`. Then the migration run via `npx migrate-mongo up` and rolled back with
+`npx migrate-mongo down`.
+
+## Testing
+
+The migrations can be run during unit tests by adding a testImplementation dependency for`test-util`
+and annotating the test class with `@QuarkusTestResource(MongoMigrationResource.class)`. The
+database name and port of the MongoDb container must also be added to `application.yml` as:
+
+- `%test.quarkus.mongodb.database`
+- `%test.quarkus.mongodb.devservices.port`
---
+++ test-util/src/main/java/com/benhession/imagepicker/testutil/resources/MongoMigrationResource.java
@@ -0,0 +1,66 @@
+package com.benhession.imagepicker.testutil.resources;
+
+import io.quarkus.test.common.QuarkusTestResourceLifecycleManager;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Map;
+import org.testcontainers.mongodb.MongoDBAtlasLocalContainer;
+import org.testcontainers.utility.DockerImageName;
+
+public class MongoMigrationResource implements QuarkusTestResourceLifecycleManager {
+
+    private MongoDBAtlasLocalContainer mongoDbContainer;
+
+    @Override
+    public Map<String, String> start() {
+        try {
+            mongoDbContainer =
+                new MongoDBAtlasLocalContainer(DockerImageName.parse("mongodb/mongodb-atlas-local:7.0.15"))
+                    .withReuse(true)
+                    .withExposedPorts(27017);
+            mongoDbContainer.start();
+
+            String databaseName = "test-db";
+            String connectionString = mongoDbContainer.getConnectionString();
+
+            ProcessBuilder pb = new ProcessBuilder();
+            pb.directory(new File("../mongo-db-migrations"));
+            pb.environment().put("MONGO_URL", connectionString);
+            pb.environment().put("DATABASE_NAME", databaseName);
+            pb.redirectErrorStream(true);
+            pb.command("npx", "migrate-mongo", "up");
+
+            Process process = pb.start();
+            int exitCode = process.waitFor();
+
+            try (BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()))) {
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    System.out.println("Migration output: " + line);
+                }
+            }
+
+            if (exitCode != 0) {
+                throw new RuntimeException("MongoDB Migration failed with exit code " + exitCode);
+            }
+
+            return Map.of(
+                "quarkus.mongodb.devservices.enabled", "false",
+                "quarkus.mongodb.database", databaseName,
+                "quarkus.mongodb.connection-string", connectionString);
+
+        } catch (IOException | InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+    @Override
+    public void stop() {
+        if (mongoDbContainer != null) {
+            mongoDbContainer.stop();
+        }
+    }
+}
